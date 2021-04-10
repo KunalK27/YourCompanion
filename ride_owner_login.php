fe435477f0e95a5ce0386e3f95c050d6ece30f20
Bhavya Shah <62424200+BhavyaShah1234@users.noreply.github.com>
2021-04-10 13:02:16 +0530

    Update ride_owner_login.php

--- ride_owner_login.php
+++ ride_owner_login.php
@@ -39,7 +39,14 @@ if(isset($_POST["ride_owner_login"]))
             // IF EMAIL AND PASSWORDS MATCH
             if($row["email"] == $email && $row["pw"] == $pw)
             {
-                // REDIRECT TO ACCOUNT
+                // LOGIN SUCCESSFUL
+                session_start();
+                $sql = "SELECT ride_owner_id FROM ride_owner WHERE email='$email'";
+                $result = $conn->query($sql);
+                while($row = $result->fetch_assoc())
+                {
+                    $_SESSION["ride_owner_id"] = $row["ride_owner_id"];
+                }
                 header("Location: ");
             }
             else
