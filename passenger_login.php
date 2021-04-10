ea215eb073b6c79f302d5bfe4f3a943edcd0cad7
Bhavya Shah <62424200+BhavyaShah1234@users.noreply.github.com>
2021-04-10 12:26:14 +0530

    Add files via upload

---
+++ passenger_login.php
@@ -0,0 +1,54 @@
+<?php
+$server = "localhost";
+$db = "car_pooling";
+$un = "root";
+$password = "";
+
+// ESTABLISHING CONNECTION
+$conn = new mysqli($server, $un, $password, $db);
+if ($conn->connect_error)
+{
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$message = "";
+
+// IF PASSENGER LOGIN IS CLICKED
+if(isset($_POST["passenger_login"]))
+{
+    // GETTING EMAIL AND PASSWORD FROM PASSENGER LOGIN FORM
+    $email = $_POST["email"];
+    $pw = $_POST["pw"];
+
+    // CHECKING IF USER EXISTS OR NOT
+    $sql = "SELECT passenger_id FROM passenger WHERE email='$email' AND pw='$pw'";
+    $result = $conn->query($sql);
+    // IF NO USER EXISTS
+    if($result->num_rows == 0)
+    {
+        // REDIRECT TO PASSENGER LOGIN PAGE
+        header("Location: ");
+        $message = "USER DOES NOT EXIST.<a href='passenger_signup.php'>SIGN UP</a> INSTEAD.";
+    }
+    // IF USER EXISTS
+    else
+    {
+        // CHECK IF EMAIL AND PASSWORD MATCH
+        while($row = $result->fetch_assoc())
+        {
+            // IF EMAIL AND PASSWORDS MATCH
+            if($row["email"] == $email && $row["pw"] == $pw)
+            {
+                // REDIRECT TO ACCOUNT
+                header("Location: ");
+            }
+            else
+            {
+                // REDIRECT TO PASSENGER LOGIN PAGE
+                header("Location: ");
+                $message = "USER DOES NOT EXIST.<a href='passenger_signup.php'>SIGN UP</a> INSTEAD.";
+            }
+        }
+    }
+}
+?>
---
+++ ride_owner_login.php
@@ -0,0 +1,54 @@
+<?php
+$server = "localhost";
+$db = "car_pooling";
+$un = "root";
+$password = "";
+
+// ESTABLISHING CONNECTION
+$conn = new mysqli($server, $un, $password, $db);
+if ($conn->connect_error)
+{
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$message = "";
+
+// IF PASSENGER LOGIN IS CLICKED
+if(isset($_POST["ride_owner_login"]))
+{
+    // GETTING EMAIL AND PASSWORD FROM RIDE OWNER LOGIN FORM
+    $email = $_POST["email"];
+    $pw = $_POST["pw"];
+
+    // CHECKING IF USER EXISTS OR NOT
+    $sql = "SELECT ride_owner_id FROM ride_owner WHERE email='$email' AND pw='$pw'";
+    $result = $conn->query($sql);
+    // IF NO USER EXISTS
+    if($result->num_rows == 0)
+    {
+        // REDIRECT TO RIDE OWNER LOGIN PAGE
+        $message = "USER DOES NOT EXIST.<a href='ride_owner_signup.php'>SIGN UP</a> INSTEAD.";
+        header("Location: ");
+    }
+    // IF USER EXISTS
+    else
+    {
+        // CHECK IF EMAIL AND PASSWORD MATCH
+        while($row = $result->fetch_assoc())
+        {
+            // IF EMAIL AND PASSWORDS MATCH
+            if($row["email"] == $email && $row["pw"] == $pw)
+            {
+                // REDIRECT TO ACCOUNT
+                header("Location: ");
+            }
+            else
+            {
+                $message = "USER DOES NOT EXIST.<a href='ride_owner_signup.php'>SIGN UP</a> INSTEAD.";
+                // REDIRECT TO RIDE OWNER LOGIN PAGE
+                header("Location: ");
+            }
+        }
+    }
+}
+?>
